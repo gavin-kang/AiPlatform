a7babf5bd05dee0f3b3e670942bed09f6598df58
venkorls <715139533@qq.com>
2018-06-14 18:00:40 +0800

    add ignore file

--- keras/xiaowan/predict.py
+++ keras/xiaowan/predict.py
@@ -14,5 +14,5 @@ x_predict=x_original.iloc[:,[1,2,3,4,5]].apply(lambda x:(x-np.min(x))/(np.max(x)
 model=load_model('./model/model.h5') #加载模型
 pre_res=pd.DataFrame(model.predict(x_predict),columns=['正常','异常']) #预测结果 并且转为dataframe
 merge_df=pd.concat([x_original,pre_res],axis=1) #合并原始数据和预测结果
-merge_df.to_csv('./data/predict.csv',encoding="gbk",index=False) #写入文件
+# merge_df.to_csv('./data/predict.csv',encoding="gbk",index=False) #写入文件
 print("succeed!")
